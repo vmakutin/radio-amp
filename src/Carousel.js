ed9253e39d842822d0a689d74d78725c633e5775
vadim.makutin <vadim.makutin@gmail.com>
2022-10-26 18:14:24 +0300

    removed auto sliding for carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,7 +5,7 @@ import './carousel.css';
 function ControlledCarousel({index, setIndex, radioStations}) {
 
   return (
-    <Carousel activeIndex={index} onSelect={(e)=>setIndex(e)}>
+    <Carousel activeIndex={index} onSelect={(e)=>setIndex(e)} data-bs-interval="false">
       
       {radioStations["radios"].map((radio) => {
         return (
